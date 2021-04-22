27cf466a6cb102ededea3883bf258de1736d099d
Jan Gosmann <jan@hyper-world.de>
2021-04-22 21:10:41 +0200

    Build documentation on docs.rs without errors

--- build.rs
+++ build.rs
@@ -15,8 +15,16 @@ rustflags = [
 ]
 ";
 
+static BINDINGS_WRITE_ERR_MSG: &str = "Could not write BSEC bindings.";
+
 fn main() {
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bsec.rs");
+
+    if let Ok(_) = std::env::var("DOCS_RS") {
+        std::fs::File::create(out_path).expect(BINDINGS_WRITE_ERR_MSG);
+        return;
+    }
+
     env::vars()
         .map(|(k, v)| format!("{}={}", k, v))
         .for_each(|s| eprintln!("{}", s));
@@ -38,6 +46,6 @@ fn main() {
         .generate()
         .expect("Unable to generate BSEC bindings.");
     bindings
-        .write_to_file(out_path.join("bsec.rs"))
-        .expect("Could not write BSEC bindings.");
+        .write_to_file(out_path)
+        .expect(BINDINGS_WRITE_ERR_MSG);
 }
