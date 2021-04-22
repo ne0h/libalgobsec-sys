fb7f1094a9dc71bc3a1b6c8962b0d28ad5db1133
Jan Gosmann <jan@hyper-world.de>
2021-04-22 19:55:00 +0200

    Initial commit of build script

---
+++ build.rs
@@ -0,0 +1,43 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+static MISSING_PATH_CONFIG_MSG: &str =
+    "The `bsec_include_path` configuration option must be set to the absolute \
+path of the BSEC header files. You can do that by creating a \
+`.cargo/config` file in your crate with the following content:
+
+[build]
+rustflags = [
+    '--cfg', 'bsec_include_path=\"/path/to/bsec/includes\"',
+    '--cfg', 'bsec_library_path=\"/path/to/bsec/includes\"',
+]
+";
+
+fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    env::vars()
+        .map(|(k, v)| format!("{}={}", k, v))
+        .for_each(|s| eprintln!("{}", s));
+    let bsec_include_path = env::var("CARGO_CFG_BSEC_INCLUDE_PATH").expect(MISSING_PATH_CONFIG_MSG);
+    let bsec_library_path = env::var("CARGO_CFG_BSEC_LIBRARY_PATH").expect(MISSING_PATH_CONFIG_MSG);
+
+    println!("cargo:rerun-if-changed={}", bsec_include_path);
+    println!("cargo:rustc-link-search=native={}", bsec_library_path);
+    println!("cargo:rustc-link-lib=static=algobsec");
+
+    let bindings = bindgen::Builder::default()
+        .header(
+            PathBuf::from(bsec_include_path)
+                .join("bsec_interface.h")
+                .to_str()
+                .unwrap(),
+        )
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate BSEC bindings.");
+    bindings
+        .write_to_file(out_path.join("bsec.rs"))
+        .expect("Could not write BSEC bindings.");
+}
